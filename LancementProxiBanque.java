51224cc838af77e0aba869b196cbcfc508fae3bf
GtmUser <patrick.duflot@gmail.com>
2019-08-05 00:31:13 +0200

    GRP2_TP1

---
+++ LancementProxiBanque.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Nacera et Sami
+ *
+ */
+public class LancementProxiBanque {
+	private static Logger logger = Logger.getLogger(LancementProxiBanque.class);
+
+	/**
+	 * 
+	 */
+	public LancementProxiBanque() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.debug("Bienvenue à ProxiBanqueSI !");
+
+	}
+
+}
